a82e1e452e728467c511cd41552367aa9b15a63e
dylanmarc <diloubilou@gmail.com>
2021-11-01 21:23:29 +1300

    fixing references

--- src/data/ProjectImagesData.js
+++ src/data/ProjectImagesData.js
@@ -1,12 +1,12 @@
-import Image1 from '../assets/photos/project-images/IMG_0010.JPG'
-import Image2 from '../assets/photos/project-images/IMG_0016.JPG'
-import Image3 from '../assets/photos/project-images/IMG_0018.JPG'
-import Image4 from '../assets/photos/project-images/IMG_0026.JPG'
-import Image5 from '../assets/photos/project-images/IMG_0027.JPG'
-import Image6 from '../assets/photos/project-images/IMG_0034.JPG'
-import Image7 from '../assets/photos/project-images/IMG_0046.JPG'
-import Image8 from '../assets/photos/project-images/IMG_0052.JPG'
-import Image9 from '../assets/photos/project-images/IMG_0056.JPG'
+import Image1 from '../assets/photos/project-images/IMG_0010.png'
+import Image2 from '../assets/photos/project-images/IMG_0016.png'
+import Image3 from '../assets/photos/project-images/IMG_0018.png'
+import Image4 from '../assets/photos/project-images/IMG_0026.png'
+import Image5 from '../assets/photos/project-images/IMG_0027.png'
+import Image6 from '../assets/photos/project-images/IMG_0034.png'
+import Image7 from '../assets/photos/project-images/IMG_0046.png'
+import Image8 from '../assets/photos/project-images/IMG_0052.png'
+import Image9 from '../assets/photos/project-images/IMG_0056.png'
 
 export const projectImagesData = [
     {
